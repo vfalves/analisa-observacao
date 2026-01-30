799200ecf4fc53336dcf7e838649590a3ddfaff3
vfalves <victorfernandoalves@yahoo.com.br>
2026-01-30 16:11:31 -0300

    ajuste de caminhos

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/analisa-observacao/',
+  base: '/analisa-observacao/', // Verifique se as barras e o nome estão corretos
 })
